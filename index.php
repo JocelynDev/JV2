149d14edd3e596e3db58a450cd147f3fa1a6c26a
JocelynDev <jocelyndevcontact@gmail.com>
2018-06-16 15:54:24 +0200

    Mise en place mascotte + Linear Gradient
    :arrow_right: Ajout d'un background-Linear Bleu
    :arrow_right: Changement de la police (Montserrat) via Google Font

--- index.php
+++ index.php
@@ -5,19 +5,79 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Jocelyn Vourron</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" media="screen" href="compiled.min.css" />  
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/compiled.min.css" />  
     <link rel="icon" type="image/png" href="rss/mascotte.png" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    
+
+    <style type="text/css">
+        body 
+        {
+            font-family: 'Montserrat', sans-serif;
+        }
+        html,
+        body,
+        header,
+        .intro-2 {
+          height: 100%;
+          
+        }
+
+        .gradienbg .rgba-gradient {
+            background : -webkit-linear-gradient(45deg,#68d0ff,#453cf5 100%);
+        }
+
+    </style>
 </head>
-<body>
+
+<body class="gradienbg">
+
+
+<header>
+    <section class="view intro-2 rgba-gradient">
+        <div class="mask">
+            <div class="container h-100 d-flex justify-content-center align-items-center">
+                <div class="row flex-center pt-5 mt-3">
+                    <div class="col-md-12 col-lg-6 text-center text-md-left margins">
+                        <div class="white-text">
+                            <center><h1 class="h1-responsive font-weight-bold wow fadeInLeft" data-wow-delay="0.3s">Bienvenue !</h1><center>
+                            <hr class="hr-light wow fadeInLeft" data-wow-delay="0.3s">
+                            <h6 class="wow fadeInLeft" data-wow-delay="0.3s">Bienvenue sur la version 2 de mon site ! Après plusieurs mois de travail pour vous proposer la meilleure expérience, le site est disponible.
+                            </h6>
+                            <br>
+                            <a class="btn btn-white btn-rounded blue-text font-weight-bold ml-lg-0 wow fadeInLeft" data-wow-delay="0.3s">Me connecter</a>
+                            <a class="btn btn-outline-white btn-rounded font-weight-bold wow fadeInLeft" data-wow-delay="0.3s">En savoir plus 
+                                </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 col-lg-6  wow fadeInRight" data-wow-delay="0.3s">
+                        <img src="rss/mascotte.png" alt="Mascotte Jocelyn Vourron" class="img-fluid" width="600" height="670">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</header>
 
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 <script src="js/compiled.min.js"></script> 
+<script>
+        //Initialisation de l'animation
+        new WOW().init();
+</script>
 </body>
 </html>
